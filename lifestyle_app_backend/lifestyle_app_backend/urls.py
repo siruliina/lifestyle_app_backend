db9bfcfc98c76c7aa9c6729d1b1163986269d473
siruliina <siruliinakarlin@hotmail.com>
2025-01-24 10:45:39 +0200

    Swagger added

--- lifestyle_app_backend/lifestyle_app_backend/urls.py
+++ lifestyle_app_backend/lifestyle_app_backend/urls.py
@@ -17,8 +17,25 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Lifestyle App",
+      default_version='v1',
+      description="Backend for Lifestyle App",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@myapi.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("users/", include("users.urls")),
+    path('users/', include("users.urls")),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='swagger-docs'),
 ]
