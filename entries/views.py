29ce4957c0f5a6ff5277b0bda34178f6f4d2b7dd
siruliina <siruliinakarlin@hotmail.com>
2025-02-12 13:15:38 +0200

    Entries can be fltered by author

--- entries/views.py
+++ entries/views.py
@@ -1,5 +1,6 @@
-# views.py
 from rest_framework import viewsets
+from rest_framework import filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Entry
 from .serializers import EntrySerializer
 
@@ -7,3 +8,7 @@ from .serializers import EntrySerializer
 class EntryViewSet(viewsets.ModelViewSet):
     queryset = Entry.objects.all()
     serializer_class = EntrySerializer
+    filter_backends = (DjangoFilterBackend, filters.OrderingFilter)
+    filterset_fields = ["author"]
+    ordering_fields = ["created_at"]
+    ordering = ["created_at"]
