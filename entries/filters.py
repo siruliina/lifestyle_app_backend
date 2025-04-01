9d775feb09fcb2128fb99b6f5247507b8eaa0467
siruliina <siruliinakarlin@hotmail.com>
2025-04-01 14:09:06 +0300

    Entries can be filtered by date, order and search

---
+++ entries/filters.py
@@ -0,0 +1,16 @@
+import django_filters
+from .models import Entry
+
+
+class EntryFilter(django_filters.FilterSet):
+    # Filtering by only date and not the time
+    created_at = django_filters.DateTimeFilter(
+        field_name="created_at", lookup_expr="date"
+    )
+
+    class Meta:
+        model = Entry
+        fields = [
+            "author",
+            "created_at",
+        ]
--- entries/views.py
+++ entries/views.py
@@ -2,13 +2,19 @@ from rest_framework import viewsets
 from rest_framework import filters
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Entry
+from .filters import EntryFilter
 from .serializers import EntrySerializer
 
 
 class EntryViewSet(viewsets.ModelViewSet):
     queryset = Entry.objects.all()
     serializer_class = EntrySerializer
-    filter_backends = (DjangoFilterBackend, filters.OrderingFilter)
-    filterset_fields = ["author"]
-    ordering_fields = ["created_at"]
-    ordering = ["created_at"]
+    filter_backends = (
+        DjangoFilterBackend,
+        filters.OrderingFilter,
+        filters.SearchFilter,
+    )
+    filterset_class = EntryFilter
+    ordering_fields = ["created_at", "title"]
+    ordering = ["created_at", "title"]
+    search_fields = ["title"]
